f8129e1e946f54f456d62dab584c3e843d0b66e6
oussamaAlouat <al317257@uji.es>
2020-07-19 01:59:19 +0200

    Used Authorization checker middleware, on routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { index } from '../controller';
 import { policiesController } from '../controller/policies.controller';
+import { checkHeaderAuthorization } from '../utils/check-header-authorization';
 
 export default (config) => {
   const routes = Router();
 
   routes.get('/',
     (req, res, next) => index(req, res, next));
-  routes.get('/policies', (req, res, next) => policiesController(req, res, next, config));
+  routes.get('/policies',
+    (req, res, next) => checkHeaderAuthorization(req, res, next),
+    (req, res, next) => policiesController(req, res, next, config));
 
   return routes;
 };
