da384f78b741263a35f1de9844db01ccd9f152d6
oussamaAlouat <al317257@uji.es>
2020-07-18 19:09:02 +0200

    Added policies route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { index } from '../controller';
+import { policiesController } from '../controller/policies.controller';
 
 export default (config) => {
   const routes = Router();
 
   routes.get('/',
     (req, res, next) => index(req, res, next));
+  routes.get('/policies', (req, res, next) => policiesController(req, res, next, config));
 
   return routes;
 };
