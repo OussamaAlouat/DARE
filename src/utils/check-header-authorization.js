d2ee2789d1bd94036fbd763be439e4bac30e6287
oussamaAlouat <al317257@uji.es>
2020-07-19 01:33:34 +0200

    Fixed code

--- src/utils/check-header-authorization.js
+++ src/utils/check-header-authorization.js
@@ -3,21 +3,21 @@
 export function checkHeaderAuthorization(req, res, next) {
   const dontRequiredPaths = ['/', '/login', '/login/'];
   if (dontRequiredPaths.includes(req.path)) {
-    //logger.debug(`req.path: ${req.path} not required paths`);
     if (req.header('Authorization')) {
       res.status(400);
-      //logger.error(`req.path: ${req.path} not required paths, but has Authorization`);
       return res.send({
         error: `400 on ${req.path} because not required Authorization`,
       });
     }
+
     return next();
   }
+
   if (req.header('Authorization')) {
     return next();
   }
+
   res.status(403);
-  //logger.error(`req.path: ${req.path} in required paths, and lacks of Authorization`);
   return res.send({
     error: `403 on ${req.path} because required Authorization`,
   });
