d4843cd1414c3909a2ccca105714a4bdc7938f02
oussamaAlouat <al317257@uji.es>
2020-07-19 01:33:51 +0200

    Renamed file

--- src/utils/handle-error.js
+++ src/utils/handle-error.js
@@ -1,8 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 
-const handleError = (req, res) => (err) => {
-  const status = err.response.data.statusCode || 500;
+const handleError = (err, req, res) => {
+  const status = err.status ? err.status : err.response.data.statusCode || 500;
   const errObject = { code: status, message: err.message };
+
   res.status(status).json(errObject);
 };
 
