620cdce77f670257c4a58f9657b79df2cd380101
oussamaAlouat <al317257@uji.es>
2020-07-19 14:04:21 +0200

    Added clients route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import { index } from '../controller';
 import { policiesController } from '../controller/policies.controller';
 import { checkHeaderAuthorization } from '../utils/check-header-authorization';
-import { sendOkResponse, processDataLimit, searchDataWithId } from '../utils/responses.util';
+import { sendOkResponse, processDataLimit, searchDataWithId, processDataWithName } from '../utils/responses.util';
+import { clientsController } from '../controller/clients.controller';
 
 export default (config) => {
   const routes = Router();
@@ -20,6 +21,13 @@ export default (config) => {
     (req, res, next) => checkHeaderAuthorization(req, res, next),
     (req, res, next) => policiesController(req, res, next, config),
     (result, req, res, next) => searchDataWithId(result, req, res, next),
+    (result, req, res, next) => sendOkResponse(result, req, res, next));
+
+  routes.get('/clients',
+    (req, res, next) => checkHeaderAuthorization(req, res, next),
+    (req, res, next) => clientsController(req, res, next, config),
+    (result, req, res, next) => processDataWithName(result, req, res, next),
+    (result, req, res, next) => processDataLimit(result, req, res, next),
     (result, req, res, next) => sendOkResponse(result, req, res, next));
 
   return routes;
