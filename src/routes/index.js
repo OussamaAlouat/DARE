2f468c322983b0e92b4ba0371dbfeff776294c4b
oussamaAlouat <al317257@uji.es>
2020-07-19 16:57:18 +0200

    Added '/clients/:id/policies' route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+
 import { index } from '../controller';
 import { policiesController } from '../controller/policies.controller';
 import { checkHeaderAuthorization } from '../utils/check-header-authorization';
-import { sendOkResponse, processDataLimit, searchDataWithId, processDataWithName } from '../utils/responses.util';
 import { clientsController } from '../controller/clients.controller';
+import {
+  sendOkResponse, processDataLimit, searchDataWithId, processDataWithName,
+  setDataInRequest, findClientPolicie, notFoundFilter,
+} from '../utils/responses.util';
 
 export default (config) => {
   const routes = Router();
@@ -34,6 +38,16 @@ export default (config) => {
     (req, res, next) => checkHeaderAuthorization(req, res, next),
     (req, res, next) => clientsController(req, res, next, config),
     (result, req, res, next) => searchDataWithId(result, req, res, next),
+    (result, req, res, next) => sendOkResponse(result, req, res, next));
+
+  routes.get('/clients/:id/policies',
+    (req, res, next) => checkHeaderAuthorization(req, res, next),
+    (req, res, next) => clientsController(req, res, next, config),
+    (result, req, res, next) => searchDataWithId(result, req, res, next),
+    (result, req, res, next) => notFoundFilter(result, req, res, next), // not found client
+    (result, req, res, next) => setDataInRequest(result, req, res, next),
+    (req, res, next) => policiesController(req, res, next, config),
+    (result, req, res, next) => findClientPolicie(result, req, res, next),
     (result, req, res, next) => sendOkResponse(result, req, res, next));
 
   return routes;
