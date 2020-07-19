24a6e03b40f0c2c782ba2b238ce36a6f5a6ec05d
oussamaAlouat <al317257@uji.es>
2020-07-19 11:52:00 +0200

    Divided all logic in some middlewares

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,19 @@ import { Router } from 'express';
 import { index } from '../controller';
 import { policiesController } from '../controller/policies.controller';
 import { checkHeaderAuthorization } from '../utils/check-header-authorization';
+import { sendOkResponse, processDataLimit } from '../utils/responses.util';
 
 export default (config) => {
   const routes = Router();
 
   routes.get('/',
     (req, res, next) => index(req, res, next));
+
   routes.get('/policies',
     (req, res, next) => checkHeaderAuthorization(req, res, next),
-    (req, res, next) => policiesController(req, res, next, config));
+    (req, res, next) => policiesController(req, res, next, config),
+    (result, req, res, next) => processDataLimit(result, req, res, next),
+    (result, req, res, next) => sendOkResponse(result, req, res, next));
 
   return routes;
 };
