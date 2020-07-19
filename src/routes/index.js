d596796f9a398eb3e4a7557869515ee3639a4852
oussamaAlouat <al317257@uji.es>
2020-07-19 12:34:21 +0200

    Added the route '/policies/:id'

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { index } from '../controller';
 import { policiesController } from '../controller/policies.controller';
 import { checkHeaderAuthorization } from '../utils/check-header-authorization';
-import { sendOkResponse, processDataLimit } from '../utils/responses.util';
+import { sendOkResponse, processDataLimit, searchDataWithId } from '../utils/responses.util';
 
 export default (config) => {
   const routes = Router();
@@ -14,6 +14,12 @@ export default (config) => {
     (req, res, next) => checkHeaderAuthorization(req, res, next),
     (req, res, next) => policiesController(req, res, next, config),
     (result, req, res, next) => processDataLimit(result, req, res, next),
+    (result, req, res, next) => sendOkResponse(result, req, res, next));
+
+  routes.get('/policies/:id',
+    (req, res, next) => checkHeaderAuthorization(req, res, next),
+    (req, res, next) => policiesController(req, res, next, config),
+    (result, req, res, next) => searchDataWithId(result, req, res, next),
     (result, req, res, next) => sendOkResponse(result, req, res, next));
 
   return routes;
