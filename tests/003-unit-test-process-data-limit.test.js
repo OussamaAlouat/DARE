c17378a2331478796871975343bc5e7659053c21
oussamaAlouat <al317257@uji.es>
2020-07-19 14:01:39 +0200

    Added new case to processDataUsingLimit function test

--- tests/003-unit-test-process-data-limit.test.js
+++ tests/003-unit-test-process-data-limit.test.js
@@ -9,3 +9,12 @@ test('-------- Process data using limit', (assert) => {
   assert.deepEqual(data[0], processedData[0]);
   assert.end();
 });
+
+test('-------- Process data using limit (limit is higger tha data length)', (assert) => {
+  const data = ['Test1', 'Test2', 'Test3'];
+  const limit = 10;
+  const processedData = processDataUsingLimit(limit, data);
+  assert.equal(data.length, processedData.length);
+  assert.deepEqual(data, processedData);
+  assert.end();
+});
