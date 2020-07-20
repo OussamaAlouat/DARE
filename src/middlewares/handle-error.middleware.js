9cfb7651e85105a5d5e3389fa23ba1a2e6c46001
oussamaAlouat <al317257@uji.es>
2020-07-20 18:10:12 +0200

    Imporved handle error middleware

--- src/middlewares/handle-error.middleware.js
+++ src/middlewares/handle-error.middleware.js
@@ -1,7 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 
 const handleError = (err, req, res) => {
-  const status = err.status ? err.status : err.response.data.statusCode || 500;
+  let status = 500;
+
+  if (err.status || err.response) {
+    status = err.status ? err.status : err.response.status;
+  }
+
   const errObject = { code: status, message: err.message };
 
   res.status(status).json(errObject);
