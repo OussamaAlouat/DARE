3329c15b3d3459614411c973df8f2722994de2c5
oussamaAlouat <al317257@uji.es>
2020-07-21 18:03:06 +0200

    Restored the test

---
+++ tests/002-unauthorized-error-code.test.js
@@ -0,0 +1,24 @@
+import test from 'tape';
+import request from 'supertest';
+import { app, server } from '../src/index';
+
+test('-------- Controller: GET /policies', (assert) => {
+  const url = '/policies';
+  const message = 'Status must be 401';
+  const responseExpected = { code: 401, message: 'Unauthorized, is required Authorization header' };
+  const statusCodeExpected = 401;
+  request(app)
+    .get(url)
+    .expect(statusCodeExpected)
+    .then(
+      (res) => {
+        assert.deepEqual(res.body, responseExpected, message);
+        assert.end();
+        server.close();
+      }, (err) => {
+        assert.fail(err.message);
+        assert.end();
+        server.close();
+      },
+    );
+});
