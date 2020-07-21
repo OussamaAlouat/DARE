02d8fb425dd6984411f63da96c9df0cbcc4a1a54
oussamaAlouat <al317257@uji.es>
2020-07-21 16:29:15 +0200

    Added obtention of external token

--- src/controller/clients.controller.js
+++ src/controller/clients.controller.js
@@ -1,6 +1,7 @@
 import { getClients } from '../api';
 import { handleError } from '../middlewares/handle-error.middleware';
-
+import { setExternalToken } from '../utils/token.util';
+import constants from '../models/constants.model';
 /* eslint-disable import/prefer-default-export */
 const clientsController = async (req, res, next, config) => {
   const token = req.headers.authorization;
@@ -8,7 +9,14 @@ const clientsController = async (req, res, next, config) => {
   return getClients(config, token).then((response) => {
     next(response.data);
   })
-    .catch((err) => handleError(err, req, res));
+    .catch(async (err) => {
+      if (err.response && constants.EXTERNAL_API_ERRORS.indexOf(err.response.staus) !== -1) {
+        await setExternalToken(req, res, config);
+        return clientsController(req, res, next, config);
+      }
+
+      return handleError(err, req, res);
+    });
 };
 
 export { clientsController };
