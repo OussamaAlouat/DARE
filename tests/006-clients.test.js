18c085708931d5d12304877bb3a320cffe7aa51c
oussamaAlouat <al317257@uji.es>
2020-07-22 00:56:04 +0200

    Added /client/:id/policies test

--- tests/006-clients.test.js
+++ tests/006-clients.test.js
@@ -96,3 +96,56 @@ test('-------- Controller: Get clients/:id ', (assert) => {
       },
     );
 });
+
+test('-------- Controller: Get clients/:id/policies ', (assert) => {
+  const config = configuration(app);
+  const url = '/login';
+  const statusCodeExpected = 200;
+  const payload = { user: config.user, password: config.password };
+
+  request(app)
+    .post(url)
+    .send(payload)
+    .expect(statusCodeExpected)
+    .then(
+      (res) => {
+        const { token } = res.body;
+        const tokenToSend = `Bearer ${token}`;
+
+        request(app)
+          .get('/clients')
+          .set('Authorization', tokenToSend)
+          .expect(statusCodeExpected)
+          .then(
+            (response) => {
+              const data = response.body;
+              const { id } = data[0];
+              request(app)
+                .get(`/clients/${id}/policies`)
+                .set('Authorization', tokenToSend)
+                .expect(statusCodeExpected)
+                .then((resp) => {
+                  const findedPolicies = resp.body;
+                  const message = 'Status should be 200 and should have the expected client';
+                  assert.deepEqual(findedPolicies.clientId, id, message);
+                  assert.end();
+                  server.close();
+                }, (err) => {
+                  assert.fail(err.message);
+                  assert.end();
+                  server.close();
+                });
+            }, (err) => {
+              assert.fail(err.message);
+              assert.end();
+              server.close();
+            },
+          );
+      }, (err) => {
+        assert.fail(err.message);
+        assert.end();
+        server.close();
+      },
+    );
+});
+
